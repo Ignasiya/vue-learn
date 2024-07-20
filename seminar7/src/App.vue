add6c3dabfcc5a8e5b8770960b3e7886df35004e
Ignasiya <ignasiya93@gmail.com>
2024-07-20 13:03:55 +0300

    Семинар 7

---
+++ seminar7/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CounterComp />
+</template>
+
+<script>
+import CounterComp from './components/CounterComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CounterComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
